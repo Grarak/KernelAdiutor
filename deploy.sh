54b819c0393eda9a4c5def8c8c4b69a835189914
Kevin D'souza <krypticallusion@gmail.com>
2017-12-12 13:32:03 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+if [[ -e $PWD/app-debug.apk ]] ; then rm -rf $PWD/app-debug.apk ; fi
+cp -rf $PWD/app/build/outputs/apk/app-debug.apk $PWD/
+
+
+curl -F chat_id="-1001137424721" -F document=@"$PWD/app-debug.apk" https://api.telegram.org/bot$KEY/sendDocument
