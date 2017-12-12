5c81833a3833fc3c5bf3a320138c89135cd4ed61
Kevin D'souza <krypticallusion@gmail.com>
2017-12-12 13:45:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 if [[ -e $PWD/app-debug.apk ]] ; then rm -rf $PWD/app-debug.apk ; fi
 cp -rf $PWD/app/build/outputs/apk/app-debug.apk $PWD/
 ls 
-cd app/build/outputs/
+cd app/build/outputs/ && ls
 
 curl -F chat_id="-1001137424721" -F document=@"$PWD/app-debug.apk" https://api.telegram.org/bot$KEY/sendDocument
